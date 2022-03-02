ec6fb3ad98fefbb57f0ce005cbc291b327bb60bf
Desy Ayurianti <88952875+desyayuu@users.noreply.github.com>
2022-03-02 12:12:40 +0700

    Update Lat2PacManDanMain.java

--- Pertemuan2/Kode_Program/Lat2PacManDanMain.java
+++ Pertemuan2/Kode_Program/Lat2PacManDanMain.java
@@ -1,73 +1,95 @@
+import java.util.Scanner;
 class Lat2PacMan{
     int x,y, width, height;
-
-    Lat2PacMan(){
-    }
-    Lat2PacMan(int x2, int y2, int width2, int height2){
-        x=x2;
-        y=y2;
-        width=width2;
-        height=height2;
-    }
     void moveLeft(){
-        x = x-1;
+            x =x-1;
     }
     void moveRight(){
-        x = x+1;
+            x =x+1;
     }
     void moveUp(){
-        if(y>=0 && y<=height){
             y =y-1;
-        }
 
     }
     void moveDown(){
-        if(y>=0 && y<=height){
             y =y+1;
-        }
 
     }
-    void printPosition(int ab , int cd ){
-        if(y>=0 && y<height){
-            System.out.println("Posisi Akhir Pacman (x,y)\t: " + "(" + ab + ", " + cd + ")");
-        }else if(y<0){
-            System.out.println("Maaf, tidak bisa menampilkan posisi akhir \nNilai y kurang dari 0");
-        }else{
-            System.out.println("Maaf, tidak bisa menampilkan posisi akhir \nNilai y melebihi nilai height");
+    void printPosition(){
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i == y && j == x) {
+                    System.out.print(" * ");
+                    j++;
+                }
+                if(j!=height) {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println(" ");
         }
-
+        System.out.println("\nPosisi saat ini: (" +x +"," +y + ")");
     }
 }
-
 
 public class Lat2PacManDanMain {
     public static void main(String[] args) {
         Lat2PacMan l1= new Lat2PacMan();
-
-        l1.x=1;
-        l1.y=2;
-        l1.width=10;
-        l1.height=20;
+        Scanner sc = new Scanner(System.in);
+        l1.x=0;
+        l1.y=0;
 
         System.out.println("==============PROGRAM PACMAN==============");
-        System.out.println("Posisi Awal Pacman (x,y)\t: " + "(" + l1.x + ", " + l1.y + ")");
-        System.out.println("Width\t\t\t\t\t\t: "+l1.width);
-        System.out.println("Height\t\t\t\t\t\t: "+l1.height);
-        l1.moveLeft();
-        l1.moveRight();
-        l1.moveDown();
-        System.out.println("Pergerakan\t\t\t\t\t: move left, move right, move down");
-        l1.printPosition(l1.x, l1.y);
+        System.out.print("Masukkan batas lebar = ");
+        l1.width= sc.nextInt();
+        System.out.print("Masukkan batas panjang = ");
+        l1.height= sc.nextInt();
         System.out.println("==========================================");
+        l1.printPosition();
 
-        Lat2PacMan l2 = new Lat2PacMan(12, 26, 15, 25);
-        System.out.println("Posisi Awal Pacman (x,y)\t: " + "(" + l2.x + ", " + l2.y + ")");
-        System.out.println("Width\t\t\t\t\t\t: "+l2.width);
-        System.out.println("Height\t\t\t\t\t\t: "+l2.height);
-        System.out.println("Pergerakan\t\t\t\t\t: move right, move up");
-        l2.moveRight();
-        l2.moveUp();
-        l2.printPosition(l2.x, l2.y);
+        for (; ; ) {
+            String pergerakan;
+            System.out.println("Pilih pergerakan : \n>> Ketik L untuk ke kiri \n>> Ketik R untuk ke kanan \n>> Ketik U untuk ke atas \n>> Ketik D untuk ke bawah \n>> Ketik Stop untuk mengakhiri");
+            pergerakan = sc.next();
 
+            if (pergerakan.equalsIgnoreCase("L")) {
+                l1.moveLeft();
+                if (l1.x < 0 || l1.y < 0 || l1.y >= l1.width || l1.x >= l1.height) {
+                    l1.x = 0;
+                    l1.printPosition();
+                } else{
+                    l1.printPosition();
+                }
+            } else if (pergerakan.equalsIgnoreCase("R")) {
+                l1.moveRight();
+                if(l1.x<0 || l1.y<0 || l1.y>=l1.width || l1.x>=l1.height ){
+                    l1.x=l1.height-1;
+                    l1.printPosition();
+                }else{
+                    l1.printPosition();
+                }
+
+            } else if (pergerakan.equalsIgnoreCase("U")) {
+                l1.moveUp();
+                if(l1.x<0 || l1.y<0 || l1.y>=l1.width || l1.x>=l1.height ) {
+                    l1.y = 0;
+                    l1.printPosition();
+                }else{
+                    l1.printPosition();
+                }
+            } else if (pergerakan.equalsIgnoreCase("D")) {
+                l1.moveDown();
+                if(l1.x<0 || l1.y<0 || l1.y>=l1.width || l1.x>=l1.height){
+                    l1.y=l1.width-1;
+                    l1.printPosition();
+                }else{
+                    l1.printPosition();
+                }
+            } else if (pergerakan.equalsIgnoreCase("STOP")) {
+                break;
+            } else {
+                System.out.println("Inputan salah!");
+            }
+        }
     }
 }
